72ba7afe7de2b06cd9cdf53b4586e0715e5beec9
Alderfer <alderfer@oll38888mac-alderfer.ad.garmin.com>
2014-07-18 12:56:03 -0500

    keyboard plugin

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 jQuery(document).ready(function($) {
+    cordova.plugins.Keyboard.disableScroll(true);
     $("#title-input").focus();
-    $("body").on('focus', '#title-input', function(event) {
-        event.preventDefault();
-        document.body.scrollTop = 0;
-    });
     $('textarea').keypress(function(e) {
         if (e.keyCode == 13) return false;
     });
