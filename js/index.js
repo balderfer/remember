2d0ae8c84bca8b99ae5863d8c088d4600dec2065
Alderfer <alderfer@oll38888mac-alderfer.ad.garmin.com>
2014-07-18 11:46:28 -0500

    auto keyboard

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 jQuery(document).ready(function($) {
     $("#title-input").focus();
-    $('textarea').keypress(function(e) {
-        if (e.keyCode == 13) return false
+    $("#title-input").blur(function(event) {
+        document.body.scrollTop = 0;
     });
-    $('#title-input').live('focus', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        window.scrollTo(0, 0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+    $('textarea').keypress(function(e) {
+        if (e.keyCode == 13) return false;
     });
 });
